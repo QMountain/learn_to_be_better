4a49ca31ef131884dfd0246c60dd3386f098da4b
qiushanfeng <qsf@dareway.com.cn>
2022-03-01 18:23:30 +0800

    commit

--- src/main/java/algorithm/leetcode/hard/IsMatch.java
+++ src/main/java/algorithm/leetcode/hard/IsMatch.java
@@ -12,22 +12,47 @@ public class IsMatch {
         if (p.contains(".*")) {
             return true;
         }
-        for (int i = 0; i < s.length(); i++) {
-            if (i > p.length()-1) {
+        if (!p.contains("*")) {
+            if (s.length() > p.length()) {
                 return false;
             }
-            if (p.charAt(i) == s.charAt(i) || p.charAt(i) == '.') {
-                continue;
-            } else if (p.charAt(i) == '*') {
-                if (p.charAt(i-1) == s.charAt(i)) {
-                    int j = i+1;
-                    //while (s.contains())
-                } else {
-                    return false;
+        }
+        int pLength = p.length();
+        int sLength = s.length();
+        int sPointer = 0;
+        int pPointer = 0;
+        boolean canContinue = true;
+        while (canContinue) {
+            if (p.charAt(pPointer) != s.charAt(sPointer)) {
+                if (p.charAt(pPointer) == '.') {
+                    pPointer++;
+                    sPointer++;
+                } else if (p.charAt(pPointer) == '*') {
+                    if (p.charAt(pPointer-1) == s.charAt(sPointer)) {
+                        sPointer++;
+                    } else {
+                        pPointer++;
+                    }
+                }else {
+                    pPointer++;
                 }
+            } else {
+                pPointer++;
+                sPointer++;
+            }
+            if (sPointer >= sLength || pPointer >= pLength) {
+                canContinue = false;
             }
         }
-        return true;
+        return sPointer == sLength;
     }
 
+    public static void main(String[] args) {
+        IsMatch isMatch = new IsMatch();
+        System.out.println(!isMatch.isMatch("aa", "a"));
+        System.out.println(isMatch.isMatch("aa", "a*"));
+        System.out.println(isMatch.isMatch("ab", ".*"));
+        System.out.println(isMatch.isMatch("aab", "c*a*b"));
+        System.out.println(isMatch.isMatch("ab", ".*c"));
+    }
 }
