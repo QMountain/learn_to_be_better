f3fa2cd1593e928b3ba86fec1b45b33907081063
qiushanfeng <qsf@dareway.com.cn>
2022-04-22 20:22:12 +0800

    commit

---
+++ src/main/java/algorithm/leetcode/easy/BitwiseComplement.java
@@ -0,0 +1,27 @@
+package algorithm.leetcode.easy;
+
+public class BitwiseComplement {
+
+    public int bitwiseComplement(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int res = 0;
+        int base = 1;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                res += base;
+            }
+            n /= 2;
+            base *= 2;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        BitwiseComplement bitwiseComplement = new BitwiseComplement();
+        System.out.println(bitwiseComplement.bitwiseComplement(10));
+        System.out.println(bitwiseComplement.bitwiseComplement(7));
+        System.out.println(bitwiseComplement.bitwiseComplement(5));
+    }
+}
