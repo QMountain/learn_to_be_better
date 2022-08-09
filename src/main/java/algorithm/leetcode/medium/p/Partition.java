0fc618b2df45693546b71e90344543e697c57e69
qiushanfeng <qiushanfeng@bobfintech.com.cn>
2022-08-09 14:55:26 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/p/Partition.java
+++ src/main/java/algorithm/leetcode/medium/p/Partition.java
@@ -2,7 +2,43 @@ package algorithm.leetcode.medium.p;
 
 import algorithm.ListNode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Partition {
+
+    // 题号 131 medium 分割回文串
+    // 动态规划存储算过的子串，减少重复计算
+    public List<List<String>> partition(String s) {
+        List<List<String>> ansList = new ArrayList<>();
+        int length = s.length();
+        if (check(s)) {
+            ansList.add(Collections.singletonList(s));
+        }
+        for (int i = 1; i < length; i++) {
+            String substring = s.substring(0, i);
+            if (check(substring)) {
+                List<List<String>> partition = partition(s.substring(i));
+                for (List<String> list : partition) {
+                    ArrayList<String> strings = new ArrayList<>(list);
+                    strings.add(0,substring);
+                    ansList.add(strings);
+                }
+            }
+        }
+        return ansList;
+    }
+
+    public boolean check(String s) {
+        int length = s.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (s.charAt(i) != s.charAt(length-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public ListNode partition(ListNode head, int x) {
         ListNode smaller = null;
@@ -40,10 +76,12 @@ public class Partition {
 
     public static void main(String[] args) {
         Partition partition = new Partition();
-        ListNode partition2 = partition.partition(null, 0);
+        System.out.println(partition.partition("bb"));
+        System.out.println(partition.partition("aab"));
+        /*ListNode partition2 = partition.partition(null, 0);
         System.out.println(partition2);
         ListNode partition1 = partition.partition(new ListNode(1, new ListNode(4, new ListNode(3,
                 new ListNode(2, new ListNode(5, new ListNode(2)))))), 3);
-        System.out.println(partition1);
+        System.out.println(partition1);*/
     }
 }
