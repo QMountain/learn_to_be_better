2adb4bce5014d0b28de2a4c092448b3e6ca8d5ad
邱山峰 <2784006954@qq.com>
2022-09-29 20:33:17 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/h/HIndex.java
+++ src/main/java/algorithm/leetcode/medium/h/HIndex.java
@@ -4,6 +4,30 @@ import java.util.Arrays;
 
 public class HIndex {
 
+    // 题号 275 H指数II
+    public int hIndexII(int[] citations) {
+        int length = citations.length;
+        if (length == 1) {
+            return citations[0] == 0 ? 0 : 1;
+        }
+        int left = 0;
+        int right = citations.length-1;
+        while (left < right) {
+            int mid = (left+right)/2;
+            int count = length - mid;
+            if (citations[mid] >= count) {
+                right = mid;
+            } else {
+                if (left == mid) {
+                    left++;
+                } else {
+                    left = mid;
+                }
+            }
+        }
+        return citations[left] >= length-left ? length-left : 0;
+    }
+
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
         int length = citations.length;
@@ -23,6 +47,11 @@ public class HIndex {
 
     public static void main(String[] args) {
         HIndex hIndex = new HIndex();
+        System.out.println(0 == hIndex.hIndexII(new int[]{0, 0}));
+        System.out.println(2 == hIndex.hIndexII(new int[]{1, 2, 100}));
+        System.out.println(3 == hIndex.hIndexII(new int[]{0, 1, 3, 5, 6}));
+
+
         System.out.println(2 == hIndex.hIndex(new int[]{2,3,2}));
         System.out.println(1 == hIndex.hIndex(new int[]{0,1,1}));
         System.out.println(2 == hIndex.hIndex(new int[]{2,2,2}));
