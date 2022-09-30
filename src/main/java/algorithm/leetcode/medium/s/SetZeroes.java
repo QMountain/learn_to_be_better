bf4f0ec5af5a164e2d0893650d0e6e852a59379c
邱山峰 <2784006954@qq.com>
2022-09-30 09:28:20 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/s/SetZeroes.java
+++ src/main/java/algorithm/leetcode/medium/s/SetZeroes.java
@@ -5,6 +5,29 @@ import java.util.Set;
 
 public class SetZeroes {
 
+    // 面试题01.08.零矩阵
+    public void setZeroes2(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] == 1 || cols[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
