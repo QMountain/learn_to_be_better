c94bfb917bdf38da3cd183b6f3829258ffae8139
邱山峰 <2784006954@qq.com>
2022-10-10 12:23:28 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/f/FindMinHeightTrees.java
+++ src/main/java/algorithm/leetcode/medium/f/FindMinHeightTrees.java
@@ -1,38 +1,61 @@
 package algorithm.leetcode.medium.f;
 
-import java.util.List;
+import java.util.*;
 
 public class FindMinHeightTrees {
 
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        int[] count = new int[n];
+        if (edges.length == 0) {
+            return Collections.singletonList(0);
+        }
+        int[] arr = new int[n];
+        List<int[]> list = new ArrayList<>();
         for (int[] edge : edges) {
-            count[edge[0]]++;
-            count[edge[1]]++;
+            list.add(edge);
+            arr[edge[0]]++;
+            arr[edge[1]]++;
         }
         while (true) {
-            int maxIndex = findMaxIndex(count);
-            int c = count[maxIndex];
-            int length = edges.length;
-            int[][] newEdges = new int[length-c][2];
-            int i = 0;
-            for (int[] edge : edges) {
-                if (edge[0] != maxIndex && edge[1] != maxIndex) {
-                    newEdges[i][0] = edge[0];
-                    newEdges[i][1] = edge[1];
+            List<int[]> nl = new ArrayList<>();
+            int[] minus = new int[n];
+            for (int[] ints : list) {
+                if (arr[ints[0]] == 1 || arr[ints[1]] == 1) {
+                    minus[ints[0]]++;
+                    minus[ints[1]]++;
+                } else {
+                    nl.add(ints);
                 }
             }
+            for (int i = 0; i < n; i++) {
+                arr[i] -= minus[i];
+            }
+            if (nl.isEmpty()) {
+                Map<Integer,Integer> map = new HashMap<>();
+                int max = 0;
+                for (int[] ints : list) {
+                    int k1NewValue = map.getOrDefault(ints[0], 0) + 1;
+                    map.put(ints[0], k1NewValue);
+                    max = Math.max(max,k1NewValue);
+                    int k2NewValue = map.getOrDefault(ints[1], 0) + 1;
+                    map.put(ints[1], k2NewValue);
+                    max = Math.max(max,k2NewValue);
+                }
+                List<Integer> ans = new ArrayList<>();
+                for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                    if (entry.getValue() == max) {
+                        ans.add(entry.getKey());
+                    }
+                }
+                return ans;
+            }
+            list = nl;
         }
     }
 
-    public int findMaxIndex(int[] count) {
-        int maxIndex = 0;
-        int length = count.length;
-        for (int i = 1; i < length; i++) {
-            if (count[i] > count[0]) {
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
+    public static void main(String[] args) {
+        FindMinHeightTrees findMinHeightTrees = new FindMinHeightTrees();
+        System.out.println(findMinHeightTrees.findMinHeightTrees(6, new int[][]{{0,1},{0,2},{0,3},{3,4},{4,5}}));
+        System.out.println(findMinHeightTrees.findMinHeightTrees(4, new int[][]{{1,0},{1,2},{1,3}}));
+        System.out.println(findMinHeightTrees.findMinHeightTrees(6, new int[][]{{3,0},{3,1},{3,2},{3,4},{5,4}}));
     }
 }
