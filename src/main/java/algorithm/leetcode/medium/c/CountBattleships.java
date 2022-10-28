d79cae76b15c332f0ceea55e6dd8d8a3d8343026
邱山峰 <2784006954@qq.com>
2022-10-28 16:50:47 +0800

    commit

---
+++ src/main/java/algorithm/leetcode/medium/c/CountBattleships.java
@@ -0,0 +1,25 @@
+package algorithm.leetcode.medium.c;
+
+public class CountBattleships {
+
+    public int countBattleships(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'X') {
+                    if (i > 0 && board[i-1][j] == 'X') {
+                        continue;
+                    }
+                    if (j > 0 && board[i][j-1] == 'X') {
+                        continue;
+                    }
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+}
