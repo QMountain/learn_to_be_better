6551639adda6a8e028d6489ee8c48de6c10f05e8
qiushanfeng <2784006954@qq.com>
2023-03-27 09:49:04 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/c/CountSubstrings.java
+++ src/main/java/algorithm/leetcode/medium/c/CountSubstrings.java
@@ -31,8 +31,49 @@ public class CountSubstrings {
         return count;
     }
 
+    // 题号：1638 统计只差一个字符的子串数目
+    // 1 <= s.length, t.length <= 100
+    // s 和 t 都只包含小写英文字母。
+    public int countSubstrings(String s, String t) {
+        int sl = s.length();
+        int tl = t.length();
+        int ans = 0;
+        // i  s的起始index
+        for (int i = 0; i < sl; i++) {
+            // j  t的起始index
+            for (int j = 0; j < tl; j++) {
+                int si = i;
+                int ti = j;
+                boolean find = false;
+                while (si < sl && ti < tl) {
+                    if (s.charAt(si) == t.charAt(ti)) {
+                        if (find) {
+                            ans++;
+                        }
+                    } else {
+                        if (find) {
+                            break;
+                        } else {
+                            find = true;
+                            ans++;
+                        }
+                    }
+                    si++;
+                    ti++;
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         CountSubstrings countSubstrings = new CountSubstrings();
+        System.out.println(10 == countSubstrings.countSubstrings("abe", "bbc"));
+        System.out.println(0 == countSubstrings.countSubstrings("a", "a"));
+        System.out.println(3 == countSubstrings.countSubstrings("ab", "bb"));
+        System.out.println(6 == countSubstrings.countSubstrings("aba", "baba"));
+
+
         System.out.println(countSubstrings.countSubstrings("aaaaa"));
         System.out.println(6 == countSubstrings.countSubstrings("aaa"));
         System.out.println(3 == countSubstrings.countSubstrings("abc"));
