651098f5ac22e3f81190f619b062f6acb1cd9e7b
qiushanfeng <2784006954@qq.com>
2023-07-02 17:24:43 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/a/AddTwoNumbers.java
+++ src/main/java/algorithm/leetcode/medium/a/AddTwoNumbers.java
@@ -6,6 +6,42 @@ import java.util.Stack;
 
 public class AddTwoNumbers {
 
+    // 题号：2 两数相加 原方法名：addTwoNumbers
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode ans = l1;
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        int carry = 0;
+        while (c1 != null || c2 != null) {
+            int v1 = 0;
+            if (c1 != null) {
+                v1 = c1.val;
+            }
+            int v2 = 0;
+            if (c2 != null) {
+                v2 = c2.val;
+            }
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            c1.val = sum % 10;
+            if (c1.next == null && c2.next == null) {
+                if (carry == 1) {
+                    c1.next = new ListNode(1);
+                }
+                return ans;
+            }
+            if (c1.next == null) {
+                c1.next = new ListNode(0);
+            }
+            c1 = c1.next;
+            if (c2.next == null) {
+                c2.next = new ListNode(0);
+            }
+            c2 = c2.next;
+        }
+        return ans;
+    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         Stack<Integer> stack1 = new Stack<>();
         while (l1 != null) {
@@ -40,6 +76,20 @@ public class AddTwoNumbers {
 
     public static void main(String[] args) {
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        ListNode listNode2 = addTwoNumbers.addTwoNumbers2(new ListNode(9,
+                        new ListNode(9, new ListNode(9,
+                                new ListNode(9, new ListNode(9,
+                                        new ListNode(9, new ListNode(9))))))),
+                new ListNode(9, new ListNode(9,
+                        new ListNode(9, new ListNode(9)))));
+        System.out.println(listNode2);
+        ListNode listNode1 = addTwoNumbers.addTwoNumbers2(new ListNode(0),
+                new ListNode(0));
+        System.out.println(listNode1);
+        ListNode listNode = addTwoNumbers.addTwoNumbers2(new ListNode(2,
+                        new ListNode(4, new ListNode(3))),
+                new ListNode(5, new ListNode(6, new ListNode(4))));
+        System.out.println(listNode);
         ListNode node = addTwoNumbers.addTwoNumbers(new ListNode(7,
                         new ListNode(2, new ListNode(4, new ListNode(3)))),
                 new ListNode(5, new ListNode(6, new ListNode(4))));
