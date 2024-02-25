400c898b89f07088d4b29eb3fab8bf6dc1e4b9d2
QM <2784006954@qq.com>
2024-02-25 21:00:32 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/l/LowestCommonAncestor.java
+++ src/main/java/algorithm/leetcode/medium/l/LowestCommonAncestor.java
@@ -6,12 +6,29 @@ import java.util.*;
 
 public class LowestCommonAncestor {
 
+    // 题号 235 二叉搜索树的最近公共祖先
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        if (root.val > p.val && root.val < q.val) {
+            return root;
+        }
+        if (root.val > q.val && root.val < p.val) {
+            return root;
+        }
+        if (root.val < p.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return lowestCommonAncestor(root.left, p, q);
+    }
+
     // 树中节点数目在范围 [2, 10^5] 内。
     // -10^9 <= Node.val <= 10^9
     // 所有 Node.val 互不相同 。
     // p != q
     // p 和 q 均存在于给定的二叉树中。
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    public TreeNode lowestCommonAncestor3(TreeNode root, TreeNode p, TreeNode q) {
         LinkedList<TreeNode> pList = new LinkedList<>();
         LinkedList<TreeNode> qList = new LinkedList<>();
         searchList(root, p, q, pList, qList);
