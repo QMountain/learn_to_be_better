1355239d55cff7d37e5d1a56e3af0319c24bf2d6
QM <2784006954@qq.com>
2024-05-30 02:07:52 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/m/MaximumLength.java
+++ src/main/java/algorithm/leetcode/medium/m/MaximumLength.java
@@ -4,8 +4,9 @@ import java.util.PriorityQueue;
 
 public class MaximumLength {
 
+    // 题号 2981 找出出现至少三次的最长特殊子字符串 I
     // 3 <= s.length <= 50
-    public int maximumLength(String s) {
+    public int maximumLength2(String s) {
         int ans = -1;
         PriorityQueue<Integer>[] arr = new PriorityQueue[26];
         int lastIndex = 0;
@@ -58,8 +59,66 @@ public class MaximumLength {
         return ans;
     }
 
+    // 题号 2982. 找出出现至少三次的最长特殊子字符串 II
+    // 3 <= s.length <= 5 * 10^5
+    public int maximumLength(String s) {
+        int ans = -1;
+        int[][] arr = new int[26][3];
+        int lastIndex = 0;
+        for (int i = 1; i < s.length(); i++) {
+            char lastChar = s.charAt(lastIndex);
+            char currChar = s.charAt(i);
+            if (currChar != lastChar) {
+                int len = i - lastIndex;
+                if (len >= arr[lastChar - 'a'][0]) {
+                    arr[lastChar - 'a'][2] = arr[lastChar - 'a'][1];
+                    arr[lastChar - 'a'][1] = arr[lastChar - 'a'][0];
+                    arr[lastChar - 'a'][0] = len;
+                } else if (len >= arr[lastChar - 'a'][1]) {
+                    arr[lastChar - 'a'][2] = arr[lastChar - 'a'][1];
+                    arr[lastChar - 'a'][1] = len;
+                } else if (len >= arr[lastChar - 'a'][2]) {
+                    arr[lastChar - 'a'][2] = len;
+                }
+                lastIndex = i;
+            }
+            if (i == s.length() - 1) {
+                int index = currChar - 'a';
+                int len;
+                if (currChar != lastChar) {
+                    len = 1;
+                } else {
+                   len = s.length() - lastIndex;
+                }
+                if (len >= arr[index][0]) {
+                    arr[index][2] = arr[index][1];
+                    arr[index][1] = arr[index][0];
+                    arr[index][0] = len;
+                } else if (len >= arr[index][1]) {
+                    arr[index][2] = arr[index][1];
+                    arr[index][1] = len;
+                } else if (len >= arr[index][2]) {
+                    arr[index][2] = len;
+                }
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            if (arr[i][0] > 2) {
+                ans = Math.max(ans, arr[i][0] - 2);
+            }
+            if (arr[i][0] + arr[i][1] >= 3 && arr[i][1] > 0) {
+                ans = Math.max(ans, Math.min(arr[i][0]-1, arr[i][1]));
+            }
+            if (arr[i][2] > 0) {
+                ans = Math.max(ans, arr[i][2]);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         MaximumLength maximumLength = new MaximumLength();
+        System.out.println(-1 == maximumLength.maximumLength("jicja"));
         System.out.println(1 == maximumLength.maximumLength("abcaba"));
         System.out.println(-1 == maximumLength.maximumLength("abcdef"));
         System.out.println(2 == maximumLength.maximumLength("aaaa"));
