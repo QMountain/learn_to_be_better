44fbc8756cfa83e2cf82b1a0057ed2bf5677b679
QM <2784006954@qq.com>
2024-06-23 14:44:37 +0800

    commit

--- src/main/java/algorithm/leetcode/easy/d/DetectCapitalUse.java
+++ src/main/java/algorithm/leetcode/easy/d/DetectCapitalUse.java
@@ -2,7 +2,27 @@ package algorithm.leetcode.easy.d;
 
 public class DetectCapitalUse {
 
+    // 1 <= word.length <= 100
+    // word 由小写和大写英文字母组成
     public boolean detectCapitalUse(String word) {
+        boolean firstCharUpper = Character.isUpperCase(word.charAt(0));
+        int countUpper = firstCharUpper ? 1 : 0;
+        for (int i = 1; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                if (countUpper < i) {
+                    return false;
+                }
+                countUpper++;
+            } else {
+                if (countUpper > 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean detectCapitalUse2(String word) {
         int length = word.length();
         if (length == 1) {
             return true;
@@ -20,6 +40,13 @@ public class DetectCapitalUse {
             return true;
         }
         return allLowerCase;
+    }
+
+    public static void main(String[] args) {
+        DetectCapitalUse detectCapitalUse = new DetectCapitalUse();
+        System.out.println(!detectCapitalUse.detectCapitalUse("aBc"));
+        System.out.println(!detectCapitalUse.detectCapitalUse("FlaG"));
+        System.out.println(detectCapitalUse.detectCapitalUse("USA"));
     }
 
 }
