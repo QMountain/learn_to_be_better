1839051c6987c1159031e722a092fff9f535161f
QM <2784006954@qq.com>
2024-06-24 14:02:20 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/n/NextGreaterElements.java
+++ src/main/java/algorithm/leetcode/medium/n/NextGreaterElements.java
@@ -12,7 +12,7 @@ public class NextGreaterElements {
         int n = nums.length;
         int[] ret = new int[n];
         Arrays.fill(ret, -1);
-        Deque<Integer> stack = new LinkedList<Integer>();
+        Deque<Integer> stack = new LinkedList<>();
         for (int i = 0; i < n * 2 - 1; i++) {
             while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
                 ret[stack.pop()] = nums[i % n];
