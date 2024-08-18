14af989467f1b593cc7ba64385a372698c665f50
QM <2784006954@qq.com>
2024-08-18 19:14:24 +0800

    commit

--- src/main/java/algorithm/leetcode/easy/c/CheckRecord.java
+++ src/main/java/algorithm/leetcode/easy/c/CheckRecord.java
@@ -3,6 +3,27 @@ package algorithm.leetcode.easy.c;
 public class CheckRecord {
 
     public boolean checkRecord(String s) {
+        int countA = 0;
+        int countContinuousL = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'A') {
+                countContinuousL = 0;
+                if (++countA >= 2) {
+                    return false;
+                }
+            } else if (c == 'L') {
+                if (++countContinuousL >= 3) {
+                    return false;
+                }
+            } else {
+                countContinuousL = 0;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkRecord2(String s) {
         if (s.contains("LLL")) {
             return false;
         }
@@ -13,4 +34,8 @@ public class CheckRecord {
         return !s.contains("A");
     }
 
+    public static void main(String[] args) {
+        CheckRecord checkRecord = new CheckRecord();
+        System.out.println(checkRecord.checkRecord("LALL"));
+    }
 }
