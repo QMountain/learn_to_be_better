931b27038d2675de1b287c94471942ef682f40d0
QM <2784006954@qq.com>
2024-09-22 18:58:56 +0800

    commit

--- src/main/java/algorithm/leetcode/easy/f/FindJudge.java
+++ src/main/java/algorithm/leetcode/easy/f/FindJudge.java
@@ -7,7 +7,43 @@ import java.util.Map;
 
 public class FindJudge {
 
+    // 1 <= n <= 1000
+    // 0 <= trust.length <= 10^4
+    // trust[i].length == 2
+    // trust 中的所有trust[i] = [ai, bi] 互不相同
+    // ai != bi
+    // 1 <= ai, bi <= n
     public int findJudge(int n, int[][] trust) {
+        int length = trust.length;
+        if (length == 0) {
+            if (n == 1) {
+                return 1;
+            }
+            return -1;
+        }
+        // 第 0 列是count多少人信任自己，-1代表自己信任了别人，不能是法官
+        int[][] record = new int[n][n+1];
+        for (int[] arr : trust) {
+            // 人的编号
+            int peopleNumber = arr[0];
+            record[peopleNumber-1][0] = -1;
+            if (record[arr[1]-1][0] == -1) {
+                continue;
+            }
+            if (record[arr[1]-1][arr[0]] == 0) {
+                record[arr[1]-1][arr[0]] = 1;
+                record[arr[1]-1][0]++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (record[i][0] == n-1) {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+
+    public int findJudge2(int n, int[][] trust) {
         int judge = -1;
         int length = trust.length;
         if (length == 0) {
@@ -40,7 +76,10 @@ public class FindJudge {
 
     public static void main(String[] args) {
         FindJudge findJudge = new FindJudge();
-        System.out.println(2 == findJudge.findJudge(2, new int[][]{{1, 2}}));
+        System.out.println(3 == findJudge.findJudge(4,
+                new int[][]{{1,3},{1,4},{2,3},{2,4},{4,3}}));
+        System.out.println(2 == findJudge.findJudge(2,
+                new int[][]{{1, 2}}));
         System.out.println(3 == findJudge.findJudge(3, new int[][]{{1, 3},{2,3}}));
     }
 }
