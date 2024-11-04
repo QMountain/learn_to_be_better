01b725a0c9468359663cd621057194ac568391c4
qiushanfeng1 <qiushanfeng1@jd.com>
2024-11-04 13:57:14 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/r/RemoveNthFromEnd.java
+++ src/main/java/algorithm/leetcode/medium/r/RemoveNthFromEnd.java
@@ -8,6 +8,23 @@ import java.util.List;
 public class RemoveNthFromEnd {
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        ListNode slow = head;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
         int size = 1;
         ListNode p = head;
         List<Integer> list = new ArrayList<>();
@@ -31,12 +48,12 @@ public class RemoveNthFromEnd {
 
     public static void main(String[] args) {
         RemoveNthFromEnd removeNthFromEnd = new RemoveNthFromEnd();
-        /*System.out.println(removeNthFromEnd.removeNthFromEnd(new ListNode(1,
+        System.out.println(removeNthFromEnd.removeNthFromEnd(new ListNode(1,
                 new ListNode(2,
                         new ListNode(3,
                                 new ListNode(4,
                                         new ListNode(5))))), 2));
-        System.out.println(removeNthFromEnd.removeNthFromEnd(new ListNode(1), 1));*/
+        System.out.println(removeNthFromEnd.removeNthFromEnd(new ListNode(1), 1));
         System.out.println(removeNthFromEnd.removeNthFromEnd(new ListNode(1,new ListNode(2)), 1));
     }
 }
