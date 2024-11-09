b16366a073759e953d1057bb4f921eb8b33f8b7b
qiushanfeng1 <qiushanfeng1@jd.com>
2024-11-09 20:44:31 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/s/SequenceReconstruction.java
+++ src/main/java/algorithm/leetcode/medium/s/SequenceReconstruction.java
@@ -1,8 +1,8 @@
 package algorithm.leetcode.medium.s;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import algorithm.Node;
+
+import java.util.*;
 
 public class SequenceReconstruction {
 
@@ -17,22 +17,60 @@ public class SequenceReconstruction {
     // sequences[i] 是 nums 的一个子序列
     public boolean sequenceReconstruction(int[] nums, int[][] sequences) {
         int n = nums.length;
-        List<HashSet<Integer>> list = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            list.add(new HashSet<>());
+        int[] indegrees = new int[n + 1];
+        Set<Integer>[] graph = new Set[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new HashSet<Integer>();
         }
         for (int[] sequence : sequences) {
-            int sl = sequence.length;
-            for (int i = 0; i < sl-1; i++) {
-                int index = sequence[i] - 1;
-                list.get(index).add(sequence[i+1]);
+            int size = sequence.length;
+            for (int i = 1; i < size; i++) {
+                int prev = sequence[i - 1], next = sequence[i];
+                if (graph[prev].add(next)) {
+                    indegrees[next]++;
+                }
             }
         }
-        for (HashSet<Integer> set : list) {
-            if (set.isEmpty()) {
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        for (int i = 1; i <= n; i++) {
+            if (indegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            if (queue.size() > 1) {
                 return false;
             }
-            if (set.size() > 1) {
+            int num = queue.poll();
+            Set<Integer> set = graph[num];
+            for (int next : set) {
+                indegrees[next]--;
+                if (indegrees[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean sequenceReconstruction2(int[] nums, int[][] sequences) {
+        int n = nums.length;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n - 1; i++) {
+            String key = nums[i] + "," + nums[i+1];
+            map.put(key, 0);
+        }
+        for (int[] sequence : sequences) {
+            int length = sequence.length;
+            for (int i = 0; i < length-1; i++) {
+                String key = sequence[i] + "," + sequence[i+1];
+                if (map.containsKey(key)) {
+                    map.put(key, map.get(key)+1);
+                }
+            }
+        }
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 0) {
                 return false;
             }
         }
