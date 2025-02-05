47ad89d639c3740a05188f15ecf07915396c96bd
qiushanfeng1 <qiushanfeng1@jd.com>
2025-02-05 09:13:16 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/s/SubsetsWithDup.java
+++ src/main/java/algorithm/leetcode/medium/s/SubsetsWithDup.java
@@ -7,6 +7,25 @@ public class SubsetsWithDup {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         Set<String> set = new HashSet<>();
+        List<List<Integer>> last = new ArrayList<>();
+        last.add(new ArrayList<>());
+        for (int num : nums) {
+            List<List<Integer>> curr = new ArrayList<>(last);
+            for (List<Integer> list : last) {
+                List<Integer> list2 = new ArrayList<>(list);
+                list2.add(num);
+                if (set.add(list2.toString())) {
+                    curr.add(list2);
+                }
+            }
+            last = curr;
+        }
+        return last;
+    }
+
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        Arrays.sort(nums);
+        Set<String> set = new HashSet<>();
         List<List<Integer>> ansList = new ArrayList<>();
         int length = nums.length;
         for (int i = 0; i < 2 << length-1; i++) {
