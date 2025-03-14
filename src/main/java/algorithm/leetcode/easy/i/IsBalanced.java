0658830cbc5a291eaa77b45df4f6dd582bfbf185
qiushanfeng1 <qiushanfeng1@jd.com>
2025-03-14 09:53:41 +0800

    commit

--- src/main/java/algorithm/leetcode/easy/i/IsBalanced.java
+++ src/main/java/algorithm/leetcode/easy/i/IsBalanced.java
@@ -4,6 +4,24 @@ import algorithm.TreeNode;
 
 public class IsBalanced {
 
+    /**
+     * 给你一个仅由数字 0 - 9 组成的字符串 num。
+     * 如果偶数下标处的数字之和等于奇数下标处的数字之和，则认为该数字字符串是一个 平衡字符串。
+     * 如果 num 是一个 平衡字符串，则返回 true；否则，返回 false。
+     */
+    public boolean isBalanced(String num) {
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < num.length(); i++) {
+            if (i % 2 == 0) {
+                a += num.charAt(i) - '0';
+            } else {
+                b += num.charAt(i) - '0';
+            }
+        }
+        return a == b;
+    }
+
     public boolean isBalanced(TreeNode root) {
         if (root == null) {
             return true;
