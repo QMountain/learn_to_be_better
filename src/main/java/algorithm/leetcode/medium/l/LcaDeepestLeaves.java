27453c21f8790eae4ab4ace1dbf11db307f491c9
qiushanfeng1 <qiushanfeng1@jd.com>
2025-04-04 10:29:39 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/l/LcaDeepestLeaves.java
+++ src/main/java/algorithm/leetcode/medium/l/LcaDeepestLeaves.java
@@ -1,4 +1,3 @@
-/*
 package algorithm.leetcode.medium.l;
 
 import algorithm.TreeNode;
@@ -9,6 +8,94 @@ import java.util.*;
 public class LcaDeepestLeaves {
 
     public TreeNode lcaDeepestLeaves(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        if (root.left == null) {
+            return lcaDeepestLeaves(root.right);
+        }
+        if (root.right == null) {
+            return lcaDeepestLeaves(root.left);
+        }
+        HashMap<TreeNode, Integer> depthMap = new HashMap<>();
+        dfs(root, depthMap);
+        return compare(root, depthMap);
+    }
+
+    public TreeNode compare(TreeNode root, HashMap<TreeNode, Integer> depthMap) {
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        if (root.left == null) {
+            return compare(root.right, depthMap);
+        }
+        if (root.right == null) {
+            return compare(root.left, depthMap);
+        }
+        Integer leftDepth = depthMap.get(root.left);
+        Integer rightDepth = depthMap.get(root.right);
+        if (Objects.equals(leftDepth, rightDepth)) {
+            return root;
+        }
+        if (leftDepth > rightDepth) {
+            return compare(root.left, depthMap);
+        }
+        return compare(root.right, depthMap);
+    }
+
+    public void dfs(TreeNode node, HashMap<TreeNode, Integer> depthMap) {
+        if (node.left == null && node.right == null) {
+            depthMap.put(node, 0);
+            return;
+        }
+        int max = 0;
+        if (node.left != null) {
+            dfs(node.left, depthMap);
+            max = Math.max(max, depthMap.get(node.left));
+        }
+        if (node.right != null) {
+            dfs(node.right, depthMap);
+            max = Math.max(max, depthMap.get(node.right));
+        }
+        depthMap.put(node, max + 1);
+    }
+
+    // 树中的节点数将在 [1, 1000] 的范围内。
+    // 0 <= Node.val <= 1000
+    // 每个节点的值都是 独一无二 的
+    public TreeNode lcaDeepestLeaves2(TreeNode root) {
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        List<TreeNode> list = new ArrayList<>();
+        list.add(root);
+        while (true) {
+            List<TreeNode> newList = new ArrayList<>();
+            for (TreeNode node : list) {
+                if (node.left != null) {
+                    newList.add(node.left);
+                    parentMap.put(node.left, node);
+                }
+                if (node.right != null) {
+                    newList.add(node.right);
+                    parentMap.put(node.right, node);
+                }
+            }
+            if (newList.isEmpty()) {
+                break;
+            }
+            list = newList;
+        }
+        Set<TreeNode> set = new HashSet<>(list);
+        while (set.size() > 1) {
+            Set<TreeNode> next = new HashSet<>();
+            for (TreeNode node : set) {
+                next.add(parentMap.get(node));
+            }
+            set = next;
+        }
+        return set.iterator().next();
+    }
+
+    public TreeNode lcaDeepestLeaves3(TreeNode root) {
         return f(root).getKey();
     }
 
@@ -29,60 +116,31 @@ public class LcaDeepestLeaves {
         return new Pair<>(root, left.getValue() + 1);
     }
 
-    // 树中的节点数将在 [1, 1000] 的范围内。
-    // 0 <= Node.val <= 1000
-    // 每个节点的值都是 独一无二 的
-    public TreeNode lcaDeepestLeaves2(TreeNode root) {
-        HashMap<TreeNode, TreeNode> parentMap = new HashMap<>();
-        parentMap.put(root, root);
-        HashMap<TreeNode, Integer> depthMap = new HashMap<>();
-        depthMap.put(root, 0);
-        addToMap(root, 0, parentMap, depthMap);
-        int maxDepth = 0;
-        List<TreeNode> list = new ArrayList<>();
-        for (Map.Entry<TreeNode, Integer> entry : depthMap.entrySet()) {
-            if (entry.getValue() > maxDepth) {
-                maxDepth = entry.getValue();
-                list.clear();
-                list.add(entry.getKey());
-            } else if (entry.getValue() == maxDepth) {
-                list.add(entry.getKey());
-            }
-        }
-        if (list.size() == 1) {
-            return list.get(0);
-        }
-        Set<TreeNode> set = new HashSet<>();
-        for (TreeNode node : list) {
-            set.add(parentMap.get(node));
-        }
-        while (set.size() > 1) {
-            Set<TreeNode> ns = new HashSet<>();
-            for (TreeNode node : set) {
-                ns.add(parentMap.get(node));
-            }
-            set = ns;
-        }
-        return new ArrayList<>(set).get(0);
-    }
-
-    public void addToMap(TreeNode root, int currDepth,
-                         HashMap<TreeNode, TreeNode> parentMap,
-                         HashMap<TreeNode, Integer> depthMap) {
-        if (root.left != null) {
-            parentMap.put(root.left, root);
-            depthMap.put(root.left, currDepth + 1);
-            addToMap(root.left, currDepth + 1, parentMap, depthMap);
-        }
-        if (root.right != null) {
-            parentMap.put(root.right, root);
-            depthMap.put(root.right, currDepth + 1);
-            addToMap(root.right, currDepth + 1, parentMap, depthMap);
-        }
-    }
 
     public static void main(String[] args) {
         LcaDeepestLeaves lcaDeepestLeaves = new LcaDeepestLeaves();
+        TreeNode treeNode5 = lcaDeepestLeaves.lcaDeepestLeaves(new TreeNode(0,
+                new TreeNode(1,
+                        null,
+                        new TreeNode(2)),
+                new TreeNode(3)));
+        TreeNode treeNode4 = lcaDeepestLeaves.lcaDeepestLeaves(new TreeNode(3,
+                new TreeNode(5,
+                        new TreeNode(6),
+                        new TreeNode(2,
+                                new TreeNode(7),
+                                new TreeNode(4))),
+                new TreeNode(1,
+                        new TreeNode(0),
+                        new TreeNode(8))));
+        TreeNode treeNode3 = lcaDeepestLeaves.lcaDeepestLeaves(new TreeNode(1,
+                new TreeNode(2,
+                        new TreeNode(3,
+                                null,
+                                new TreeNode(6)),
+                        new TreeNode(4,
+                                null,
+                                new TreeNode(5))), null));
         TreeNode treeNode2 = lcaDeepestLeaves.lcaDeepestLeaves(new TreeNode(0,
                 new TreeNode(1, null, new TreeNode(2)),
                 new TreeNode(3)));
@@ -98,4 +156,3 @@ public class LcaDeepestLeaves {
         System.out.println(treeNode);
     }
 }
-*/
