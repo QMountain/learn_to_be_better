b7edb0b1cc99f6486bd6bb7ec80a1983f4921ff4
qiushanfeng1 <qiushanfeng1@jd.com>
2025-05-06 09:36:38 +0800

    commit

---
+++ src/main/java/algorithm/leetcode/easy/b/BuildArray.java
@@ -0,0 +1,14 @@
+package algorithm.leetcode.easy.b;
+
+public class BuildArray {
+
+    public int[] buildArray(int[] nums) {
+        int length = nums.length;
+        int[] ans = new int[length];
+        for (int i = 0; i < length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+
+}
