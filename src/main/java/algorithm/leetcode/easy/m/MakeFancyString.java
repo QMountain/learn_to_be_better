fc022e2f5dec031bbeacd3dcb7540bbedd824bf9
qiushanfeng1 <qiushanfeng1@jd.com>
2025-07-21 10:48:15 +0800

    commit

--- src/main/java/algorithm/leetcode/easy/m/MakeFancyString.java
+++ src/main/java/algorithm/leetcode/easy/m/MakeFancyString.java
@@ -4,6 +4,31 @@ public class MakeFancyString {
 
     public String makeFancyString(String s) {
         StringBuilder sb = new StringBuilder();
+        char preChar = ' ';
+        int count = 0;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (c == preChar) {
+                if (++count < 3) {
+                    sb.append(c);
+                }
+            } else {
+                count = 1;
+                preChar = c;
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        MakeFancyString makeFancyString = new MakeFancyString();
+        System.out.println(makeFancyString.makeFancyString("leeetcode"));
+    }
+
+    public String makeFancyString2(String s) {
+        StringBuilder sb = new StringBuilder();
         int length = s.length();
         for (int i = 0; i < length; i++) {
             char c = s.charAt(i);
