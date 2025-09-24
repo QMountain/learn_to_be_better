8969bb86930cf3636c499a1384be2624420a23b1
qiushanfeng1 <qiushanfeng1@jd.com>
2025-09-24 20:00:14 +0800

    commit

--- src/main/java/algorithm/leetcode/medium/m/MinSubArrayLen.java
+++ src/main/java/algorithm/leetcode/medium/m/MinSubArrayLen.java
@@ -5,7 +5,45 @@ import java.util.LinkedList;
 
 public class MinSubArrayLen {
 
+    /**
+     * LCR 008. 长度最小的子数组
+     * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+     * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，
+     * 并返回其长度。如果不存在符合条件的子数组，返回 0 。
+     * 1 <= target <= 10^9
+     * 1 <= nums.length <= 10^5
+     * 1 <= nums[i] <= 10^5
+     * 进阶：
+     * 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。
+     */
     public int minSubArrayLen(int target, int[] nums) {
+        int length = nums.length;
+        int left = 0;
+        int sum = 0;
+        int ans = 0;
+        for (int i = 0; i < length; i++) {
+            sum += nums[i];
+            if (sum >= target) {
+                if (ans == 0) {
+                    ans = i - left + 1;
+                } else {
+                    ans = Math.min(ans, i - left + 1);
+                }
+                for (int j = left; j < i; j++) {
+                    sum -= nums[left++];
+                    if (sum < target) {
+                        break;
+                    } else {
+                        ans = Math.min(ans, i - left + 1);
+                    }
+                }
+
+            }
+        }
+        return ans;
+    }
+
+    public int minSubArrayLen3(int target, int[] nums) {
         int length = nums.length;
         int startIndex = 0;
         int endIndex = 0;
