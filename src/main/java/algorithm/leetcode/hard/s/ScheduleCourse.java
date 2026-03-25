bf08180e09110398c9f62516261a7e359a23f49d
qiushanfeng1 <qiushanfeng1@jd.com>
2026-03-25 17:42:18 +0800

    commit

--- src/main/java/algorithm/leetcode/hard/s/ScheduleCourse.java
+++ src/main/java/algorithm/leetcode/hard/s/ScheduleCourse.java
@@ -1,71 +1,69 @@
-package algorithm.leetcode.hard;
+package algorithm.leetcode.hard.s;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
-/**
- * @ClassName ScheduleCourse
- * @Description 课程表Ⅲ
- * @Author qsf
- * Date   2021-12-14  0:12
- */
 public class ScheduleCourse {
 
+    /**
+     * 630. 课程表 III
+     * 这里有 n 门不同的在线课程，按从 1 到 n 编号。
+     * 给你一个数组 courses ，其中 courses[i] = [durationi, lastDayi]
+     * 表示第 i 门课将会 持续 上 durationi 天课，并且必须在不晚于 lastDayi 的时候完成。
+     * 你的学期从第 1 天开始。且不能同时修读两门及两门以上的课程。
+     * 返回你最多可以修读的课程数目。
+     * 1 <= courses.length <= 10^4
+     * 1 <= durationi, lastDayi <= 10^4
+     */
     public int scheduleCourse(int[][] courses) {
-        List<Integer> needDelete = new ArrayList<>(courses.length/3);
-        for (int i = 0; i < courses.length; i++) {
-            if (courses[i][0] > courses[i][1]) {
-                needDelete.add(i);
+        // 按照课程的截止日期排序
+        Arrays.sort(courses, Comparator.comparingInt(a -> a[1]));
+        
+        // 使用最大堆来存储已选课程的持续时间
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        
+        int currentTime = 0;
+        int courseCount = 0;
+        
+        for (int[] course : courses) {
+            int duration = course[0];
+            int lastDay = course[1];
+            
+            // 如果当前课程可以选
+            if (currentTime + duration <= lastDay) {
+                currentTime += duration;
+                maxHeap.offer(duration);
+                courseCount++;
+            } else if (!maxHeap.isEmpty() && maxHeap.peek() > duration) {
+                // 如果当前课程不能选，但之前选的最长课程比当前课程长
+                // 则替换掉最长的课程
+                currentTime = currentTime - maxHeap.poll() + duration;
+                maxHeap.offer(duration);
             }
         }
-        int[][] newCourses = new int[courses.length-needDelete.size()][2];
-        if (needDelete.size() == 0) {
-            newCourses = courses;
-        } else {
-            int j = 0;
-            for (int i = 0; i < courses.length; i++) {
-                if (!needDelete.contains(i)) {
-                    newCourses[j][0] = courses[i][0];
-                    newCourses[j][1] = courses[i][1];
-                    j++;
-                }
-            }
-        }
-        if (newCourses.length == 0) {
-            return 0;
-        }
-        if (newCourses.length == 1) {
-            if (newCourses[0][0] > newCourses[0][1]) {
-                return 0;
-            }
-            return 1;
-        }
-
-        Map<Integer,Integer> priorityMap = new TreeMap<>();
-        for (int[] newCourse : newCourses) {
-            priorityMap.put(newCourse[1], newCourse[0]);
-        }
-        int res = 0;
-        int finishDay = 0;
-        for (Map.Entry<Integer, Integer> entry : priorityMap.entrySet()) {
-            Integer lastDay = entry.getKey();
-            Integer duration = entry.getValue();
-            if (finishDay + duration > lastDay) {
-                return res;
-            }
-            finishDay += duration;
-            res++;
-        }
-        return res;
+        
+        return courseCount;
     }
 
     public static void main(String[] args) {
         ScheduleCourse scheduleCourse = new ScheduleCourse();
-        /*System.out.println(scheduleCourse.scheduleCourse(new int[][]{{100, 200}, {200, 1300}, {1000, 1250}, {2000, 3200}}));
-        System.out.println(scheduleCourse.scheduleCourse(new int[][]{{1, 2}}));
-        System.out.println(scheduleCourse.scheduleCourse(new int[][]{{3, 2},{4,3}}));*/
-        System.out.println(scheduleCourse.scheduleCourse(new int[][]{{5, 5},{4,6},{2,6}}));
+        // 测试用例1
+        System.out.println("测试用例1: " + scheduleCourse.scheduleCourse(new int[][]{{100, 200}, {200, 1300}, {1000, 1250}, {2000, 3200}}));
+        // 预期结果: 3 (选择课程 [100,200], [200,1300], [1000,1250])
+
+        // 测试用例2
+        System.out.println("测试用例2: " + scheduleCourse.scheduleCourse(new int[][]{{1, 2}}));
+        // 预期结果: 1
+
+        // 测试用例3
+        System.out.println("测试用例3: " + scheduleCourse.scheduleCourse(new int[][]{{3, 2},{4,3}}));
+        // 预期结果: 0 (两门课都无法完成)
+
+        // 测试用例4
+        System.out.println("测试用例4: " + scheduleCourse.scheduleCourse(new int[][]{{5, 5},{4,6},{2,6}}));
+        // 预期结果: 2 (选择课程 [2,6] 和 [4,6])
+
+        // 测试用例5
+        System.out.println("测试用例5: " + scheduleCourse.scheduleCourse(new int[][]{{7,17},{3,12},{10,20},{9,10},{5,20},{7,19},{4,18}}));
+        // 预期结果: 4
     }
 }
